9d4c46da092a5c330907b555a6fb79e773ea5417
pramudnadula <pramudn@gmail.com>
2022-11-13 12:31:36 +0530

    Edit View Delete
    Signed-off-by: pramudnadula <pramudn@gmail.com>

--- src/features/marianResources/MarianResourcesHome.js
+++ src/features/marianResources/MarianResourcesHome.js
@@ -1,8 +1,24 @@
-import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native"
+import { display } from '@mui/system';
+import { useEffect, useState } from 'react';
+import { Image, Pressable, ScrollView, StyleSheet, Text, View } from "react-native"
 import NavigationBottomBar from "../../components/NavigationBottomBar";
 import SideBar from "../../components/SideBar";
+import { GET } from '../../helpers/httphelper';
 
 function MarianResourcesHome({ navigation }) {
+    const [marianResources, setMarianResources] = useState([]);
+
+    useEffect(() => {
+        //get all resources
+        getAll();
+    }, [])
+
+    //get all resources
+    const getAll = async () => {
+        await GET("api/marianResources/getMarianResources").then((res) => {
+            setMarianResources(res.marianResources);
+        });
+    }
     return (
         <View style={styles.container} >
             {/* Side Bar */}
@@ -12,22 +28,71 @@ function MarianResourcesHome({ navigation }) {
                 Marian Resources
             </Text>
             {/* Content Here */}
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <View style={styles.content}>
-                    <Text >
-                        Marian Resources Home Page
-                    </Text>
+            <ScrollView showsVerticalScrollIndicator={false}
+                style={{
+                    marginBottom: '20%',
+                }}
+            >
+                <View style={styles.contentColumn}>
+
+                    {/* MarianResourcesViewUser */}
+                    {marianResources ? marianResources?.map((marianResource, index) => {
+                        return (
+                            <Pressable style={styles.viewMarianResources} key={index}
+                                onPress={() => navigation.navigate("MarianResourcesViewUser", { marianResource: marianResource })}
+                            >
+
+                                <Text
+                                    style={{
+                                        fontWeight: "bold",
+                                        fontSize: 20,
+                                    }}
+                                > {marianResource?.title}</Text>
+                                <Image
+                                    source={{ uri: marianResource?.image }}
+                                    style={{
+                                        width: '100%',
+                                        height: 100,
+                                        borderRadius: 10,
+                                        backgroundColor: "#82C7EE",
+
+                                    }} />
+                                <Text
+                                    style={{
+                                        fontSize: 15,
+                                        textAlign: "justify",
+                                        height: '28%',
+                                    }}
+                                >{marianResource?.details}</Text>
+                                <Text
+                                    style={{
+                                        fontSize: 15,
+                                        bottom: 25,
+                                        right: 10,
+                                        position: 'absolute',
+                                    }}
+                                >
+                                    {marianResource?.author}
+                                </Text>
+                                <Text
+                                    style={{
+                                        fontSize: 15,
+                                        bottom: 10,
+                                        right: 10,
+                                        position: 'absolute',
+                                    }}
+                                >
+                                    {(marianResource?.date).split('T')[0]}
+
+                                </Text>
+                            </Pressable>
+                        )
+                    }) : null}
                 </View>
-                {/* MarianResourcesViewUser */}
-                <Pressable style={styles.viewMarianResources} onPress={() => navigation.navigate("MarianResourcesViewUser")}>
-                    <Text style={styles.viewMarianResourcesText}>
-                        view user marian resources
-                    </Text>
-                </Pressable>
-            </ScrollView>
+            </ScrollView >
             {/* Bottom Navigation Bar */}
-            <NavigationBottomBar navigation={navigation} />
-        </View>
+            < NavigationBottomBar navigation={navigation} />
+        </View >
     )
 }
 
@@ -45,18 +110,36 @@ const styles = StyleSheet.create({
         margin: 30,
         justifyContent: "flex-start"
     },
+    contentColumn: {
+        height: "100%",
+        // width: "100%",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        overflow: "scroll",
+        justifyContent: "space-evenly",
+    },
     viewMarianResources: {
-        backgroundColor: "#82C7EE",
+        width: 185,
+        height: 250,
+        margin: 5,
+        // backgroundColor: "#82C7EE",
         padding: 10,
         borderRadius: 25,
         borderWidth: 1,
-        borderColor: "#82C7EE",
-        alignItems: "center",
-        justifyContent: "center",
+        // shadow
+        // shadowColor: "#000",
+        // shadowOffset: {
+        //     width: 0,
+        //     height: 2,
+        // },
+        // shadowOpacity: 0.25,
+        // shadowRadius: 3.84,
+        // elevation: 5,
+
+        // borderColor: "#82C7EE",
     },
-    viewMarianResourcesText: {
-        color: "#fff",
-        fontSize: 16
-    },
+    // viewMarianResourcesText: {
+    //     color: "#fff",
+    // },
 
 });
